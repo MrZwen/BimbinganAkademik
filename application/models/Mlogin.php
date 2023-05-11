a7c8707487eaf2df9cc8a95ed3fd0b0f34e09098
kr1aas <krisnaastika71@gmail.com>
2023-05-11 17:16:09 +0800

    commentnya ketingalan

--- application/models/Mlogin.php
+++ application/models/Mlogin.php
@@ -6,12 +6,12 @@ class Mlogin extends CI_Model
         $username=$this->input->post('username');
         $password=$this->input->post('password');
 
-        $sql="select * from akun INNER JOIN mahasiswa ON akun.id_akun = mahasiswa.id_akun where username='".$username."'and password='".$password."'";
+        $sql="select * from akun INNER JOIN mahasiswa ON akun.id_akun = mahasiswa.id_akun where username='".$username."'and password='".$password."'"; //merubah nim jadi username dan melakukan inner join agar nama mahasiswa yang tampil pada dashboard
         $query=$this->db->query($sql);
         if ($query->num_rows()>0)
         { 
             $data=$query->row();
-                $Nama=$data->Nama; //Harusnya Username cuma belum ada
+                $Nama=$data->Nama; //sudah ku ubah jadi nama yang ada pada table mahasiswa
 				$Role=$data->Role;
 				$array=array(
                     'Nama'=>$Nama,
