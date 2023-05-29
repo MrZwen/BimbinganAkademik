c015b261823b496dbd27a498fad95352103d4c31
DwikiChristian <133040158+DwikiChristian@users.noreply.github.com>
2023-05-29 22:16:16 +0800

    uploadfotodosen

--- application/controllers/Cdatadiri.php
+++ application/controllers/Cdatadiri.php
@@ -39,5 +39,28 @@
         }
         
     }
+
+    public function uploadDosen() {
+        $config['upload_path']   = 'gambar/'; // Lokasi penyimpanan foto (pastikan folder telah ada)
+        $config['allowed_types'] = 'jpg|jpeg|png'; // Jenis file yang diizinkan
+        $config['max_size']      = 2048; // Ukuran file maksimum dalam kilobita
+
+        $this->load->library('upload', $config);
+
+        if (!$this->upload->do_upload('photo')) {
+            // Jika gagal mengunggah foto, tampilkan pesan error
+            $error = array('error' => $this->upload->display_errors());
+            print_r($error);
+        } else {
+            $data = array('upload_data' => $this->upload->data());
+
+            // Ambil nama file yang diunggah
+            $file_name = $data['upload_data']['file_name'];
+
+            // Simpan nama file ke dalam kolom database (misalnya, menggunakan model atau metode lainnya)
+            $this->mdatadiri->saveFileD($file_name);
+        }
+        
+    }
 } 
 ?>
