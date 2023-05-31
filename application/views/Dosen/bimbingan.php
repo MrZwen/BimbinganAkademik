c807fad8d5f29ba2785286105a978ca7a44a333f
DwikiChristian <133040158+DwikiChristian@users.noreply.github.com>
2023-05-31 22:28:18 +0800

    hehe

--- application/views/Dosen/bimbingan.php
+++ application/views/Dosen/bimbingan.php
@@ -40,8 +40,8 @@
     <td>
         <!-- Modal -->
         <button type="button" class="btn btn-success btn-sm" onclick="">Reply</button>
-        <button type="button" class="btn btn-primary" onclick="" style="" data-loading-text="Memuat..." data-toggle="modal" data-target="#editProfile"><i class="fa fa-edit"></i>Open</button>
-            <div class="modal fade" id="editProfile" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <button type="button" class="btn btn-primary" onclick=""data-loading-text="Memuat..." data-toggle="modal" data-target="#Open"><i class="fa fa-edit"></i>Open</button>
+            <div class="modal fade" id="Open" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
@@ -50,6 +50,32 @@
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
+          <div class="modal-body">
+           <form id="identitas" name="identitas" method="post" action="">
+                  <label for="">NIM</label>
+                  <input id="Nim" placeholder="" name="Nim" type="text" value="<?php echo $data->Nim ?>"class="form-control mb-3">
+                  <label for="">Nama Mahasiswa</label>
+                  <input id="Nama" placeholder="" name="Nama" type="text" value="<?php echo $data->Nama ?>" class="form-control mb-3">
+                <div class="row">
+                <div class="col-md-6">
+                        <label for="">Handphone</label>
+                        <input id="NoHP" placeholder="" name="NoHP" type="number" value="" class="form-control mb-3">
+                    </div>
+                    <div class="col-md-6">
+                        <label for="">Email</label>
+                        <input type="email" id="Email" placeholder="" name="Email" value="<?php echo $data->Email ?>" class="form-control mb-3">
+                    </div>
+                </div>
+                <label for="">Alamat</label>
+                <input id="Alamat" placeholder="" name="Alamat" type="text" value="" class="form-control mb-3">
+                <input type="hidden" name="id_akun" value="<?= $this->session->userdata('id_akun') ?>" id="id_akun" />
+
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" onclick="prosessimpan();">Update</button>
+        </div>
+        </form>
      	
     </td>
   <?php
