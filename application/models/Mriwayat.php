a9b76fbd0bf1ed2837fd64b480993503c7de3735
kr1aas <krisnaastika71@gmail.com>
2023-06-05 18:47:48 +0800

    salah code

--- application/models/Mriwayat.php
+++ application/models/Mriwayat.php
@@ -4,8 +4,9 @@ class Mriwayat extends CI_Model
     function riwayatM()
     {
         $id_akun = $this->session->userdata('id_akun');
+        $this->db->where('id_akun', $id_akun);
         $query = $this->db->get('riwayat');
-        $this->db->where('mahasiswa.id_akun', $id_akun);
+        
 
         if ($query->num_rows() > 0) {
             $hasil = $query->result();
