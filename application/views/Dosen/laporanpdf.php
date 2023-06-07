59032ad7e5ce49901a0cc3b40e22a82731388ff8
DwikiChristian <133040158+DwikiChristian@users.noreply.github.com>
2023-06-08 00:01:11 +0800

    pdf

--- application/views/Dosen/laporanpdf.php
+++ application/views/Dosen/laporanpdf.php
@@ -27,20 +27,21 @@
  */
 
 // Include the main TCPDF library (search for installation path).
-//require_once('tcpdf_include.php');
+include_once('TCPDF/tcpdf.php');
 
 // create new PDF document
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
 // set document information
 $pdf->setCreator(PDF_CREATOR);
-$pdf->setAuthor('Nicola Asuni');
-$pdf->setTitle('TCPDF Example 005');
+$pdf->setAuthor('Politeknik Negeri Bali');
+$pdf->setTitle('Laporan Bimbingan');
 $pdf->setSubject('TCPDF Tutorial');
 $pdf->setKeywords('TCPDF, PDF, example, test, guide');
 
 // set default header data
-$pdf->setHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 005', PDF_HEADER_STRING);
+$pdf->setHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.'', PDF_HEADER_STRING);
+
 
 // set header and footer fonts
 $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
@@ -70,7 +71,6 @@ if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
 
 // set font
 $pdf->setFont('times', '', 10);
-
 // add a page
 $pdf->AddPage();
 
@@ -85,10 +85,32 @@ $pdf->setFillColor(255, 255, 127);
 
 // MultiCell($w, $h, $txt, $border=0, $align='J', $fill=0, $ln=1, $x='', $y='', $reseth=true, $stretch=0, $ishtml=false, $autopadding=true, $maxh=0)
 
+
+
 $tittle = <<<EOD
-<h3>Riwayat Bimbingan</h3>
+<h2 style="text-weight:bold">Riwayat Bimbingan</h2>
+<br>
+<table>
+<tr>
+<th>Nama    : </th>
+
+<th>Nama Orang Tua : </th>
+</tr>
+<tr>
+<td>Nim : </td> 
+<td>Alamat Orang Tua :</td>
+</tr>
+<tr>
+<td>Alamat :</td>
+<td>No.Hp Orang Tua :</td>
+</tr>
+<tr>
+<td>NO.Hp :</td>
+<td>Alamat Pembimbing :</td>
+</tr>
+</table>
 EOD; 
-$pdf->writeHTMLCell(0, 0, '','', $tittle, 0, 1, 0, true, 'C', true);
+$pdf->writeHTMLCell(0, 0, '','', $tittle, 0, 1, 0, true, 'L', true);
 
 $table = '<table style="border:1px solid #000; padding:6px;">';
 
